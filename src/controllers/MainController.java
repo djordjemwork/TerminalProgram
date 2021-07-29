6816bc62b9bd26223649dda8e9406e631b7e0833
Djordje Milenkovic <djordje.milenkovic@vlatacom.com>
2021-07-29 17:27:56 +0200

    Added listview in main window

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,5 +1,19 @@
 package controllers;
 
-public class MainController {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+
+        } catch (Exception ex) {
+
+        }
+    }
 }
