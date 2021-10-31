70e228529b8d191d3b42748bf9be8e8a9ee25383
Djordje Milenkovic Home Laptop <djordje.milenkovic@vlatacom.com>
2021-10-31 15:44:27 +0100

    Fixed confl

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -40,7 +40,6 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-<<<<<<< Updated upstream
         columnUserName.setCellValueFactory(new PropertyValueFactory<>("username"));
         columnUserName.setCellFactory(TextFieldTableCell.forTableColumn());
         columnPassword.setCellValueFactory(new PropertyValueFactory<>("password"));
@@ -85,71 +84,6 @@ public class MainController implements Initializable {
                     tableUserAccounts.getItems().add(new UserAccount(txtUserName.getText(),
                             checkShowPassword.isSelected() ? txtPassword.getText() : "************", false,
                             txtPassword.getText()));
-=======
-        try {
-            columnUserName.setCellValueFactory(new PropertyValueFactory<>("username"));
-            columnUserName.setCellFactory(TextFieldTableCell.forTableColumn());
-            columnPassword.setCellValueFactory(new PropertyValueFactory<>("password"));
-            columnPassword.setCellFactory(TextFieldTableCell.forTableColumn());
-
-            ReadDataFromCardService readDataFromCardService = new ReadDataFromCardService();
-            ReadDataFromCard readDataFromCard = new ReadDataFromCard(readDataFromCardService, tableUserAccounts);
-            readDataFromCard.handle(new ActionEvent());
-
-            btnDeleteAccount.setOnAction(event -> {
-                ObservableList<UserAccount> sel, items;
-                items = tableUserAccounts.getItems();
-                sel = tableUserAccounts.getSelectionModel().getSelectedItems();
-                for (UserAccount m : sel)
-                    items.remove(m);
-                tableUserAccounts.getSelectionModel().clearSelection();
-            });
-
-            btnAddNewAccount.setOnAction(event -> {
-                // Create Dialog form
-                Dialog<Pair<String, String>> dialog = new Dialog<>();
-                dialog.setTitle("Add new account");
-                ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
-                dialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
-                GridPane gridPane = new GridPane();
-                gridPane.setHgap(10);
-                gridPane.setVgap(10);
-                gridPane.setPadding(new Insets(20, 150, 10, 10));
-                TextField txtUserName = new TextField();
-                txtUserName.setPromptText("Username");
-                TextField txtPassword = new TextField();
-                txtPassword.setPromptText("Password");
-                gridPane.add(new Label("Username: "), 0, 0);
-                gridPane.add(txtUserName, 1, 0);
-                gridPane.add(new Label("Password: "), 0, 1);
-                gridPane.add(txtPassword, 1, 1);
-                dialog.getDialogPane().setContent(gridPane);
-
-
-                dialog.setResultConverter(dialogButton -> {
-                    if (dialogButton == okButtonType) {
-                        tableUserAccounts.getItems().add(new UserAccount(txtUserName.getText(),
-                                checkShowPassword.isSelected() ? txtPassword.getText() : "************", false,
-                                txtPassword.getText()));
-                    }
-                    return null;
-                });
-                dialog.showAndWait();
-            });
-
-            SaveToCardCardService saveToCardCardService = new SaveToCardCardService();
-            btnSaveToCard.setOnAction(new SaveToCardClick(progressBarMain, tableUserAccounts, saveToCardCardService));
-
-            tableUserAccounts.setRowFactory((tv) -> new TableRow<UserAccount>() {
-                @Override
-                public void updateItem(UserAccount item, boolean empty) {
-                    super.updateItem(item, empty);
-                    if (item != null) {
-                        setStyle("");
-                    }
-                    if (item != null && !item.isSavedToCard()) {
-                        setStyle("-fx-text-background-color: red;");
-                    }
                 }
                 return null;
             });
