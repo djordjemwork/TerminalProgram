d620641c4f23505f1e2314d0e9e81d81e2c774a5
Djordje Milenkovic <djordje.milenkovic@vlatacom.com>
2021-11-22 11:55:33 +0100

    Modified exception dialog window

--- src/application/Util.java
+++ src/application/Util.java
@@ -1,13 +1,6 @@
 package application;
 
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 public class Util {
 
@@ -17,30 +10,7 @@ public class Util {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Exception Dialog");
         alert.setHeaderText("There was a problem");
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        ex.printStackTrace(pw);
-        String exceptionText = ex.getLocalizedMessage();
-
-        Label label = new Label("The exception stacktrace was:");
-
-        TextArea textArea = new TextArea(exceptionText);
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
-
-        textArea.setMaxWidth(Double.MAX_VALUE);
-        textArea.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setVgrow(textArea, Priority.ALWAYS);
-        GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-        GridPane expContent = new GridPane();
-        expContent.setMaxWidth(Double.MAX_VALUE);
-        expContent.add(label, 0, 0);
-        expContent.add(textArea, 0, 1);
-
-        alert.getDialogPane().setExpandableContent(expContent);
-
+        alert.setContentText(ex.getCause().getMessage());
         alert.show();
     }
 
